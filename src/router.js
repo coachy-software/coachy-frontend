e8869b475b842d25e48c66f8f7eb4015d1c58627
bmstefanski <bstefanskicom@gmail.com>
2019-01-11 10:10:35 +0100

    Fix landing page home link

--- src/router.js
+++ src/router.js
@@ -36,7 +36,7 @@ export default new Router({
         {
           path: '',
           name: 'landing',
-          component: () => import('@/views/landing/HomeView')
+          component: () => import('@/views/landing/home/HomeView')
         }
       ]
     },
