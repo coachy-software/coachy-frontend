50f61967b86ca2d838d8ebc85c34b0cb22993f92
bmstefanski <bstefanskicom@gmail.com>
2019-01-19 18:03:28 +0100

    Improve menu component

--- src/views/dashboard/component/Menu.vue
+++ src/views/dashboard/component/Menu.vue
@@ -1,16 +1,43 @@
 <template lang="pug">
   .col-lg.order-lg-first
     ul.nav.nav-tabs.border-0.flex-column.flex-lg-row
-      li.nav-item
-        a.nav-link.active
-          i.fe.fe-home
-          |  Home
-      li.nav-item
-        a.nav-link
-          i.fe.fe-box
-          |  Interface
-        .dropdown-menu.dropdown-menu-arrow
-          a.dropdown-item Example
-          a.dropdown-item Example
-          a.dropdown-item Example
+      div(v-for="item in menu")
+        li.nav-item(v-if="item.type === 'NORMAL'")
+          a.nav-link.active
+            i(:class="item.icon")
+            |  Home
+        div(v-if="item.type === 'DROPDOWN'")
+          li.nav-item(@mouseover="showDropdown(item.name)", @mouseleave="hideDropdown(item.name)", :id="item.name")
+            a.nav-link
+              i(:class="item.icon")
+              |  {{item.name}}
+            .dropdown-menu(:id="'menu' + item.name")
+              div(v-for="dropdownItem in item.dropdown")
+                a.dropdown-item {{dropdownItem}}
 </template>
+
+<style scoped>
+  .dropdown-menu {
+    margin-top: 0;
+  }
+</style>
+
+<script>
+  import DashboardMenu from '@/assets/menu/dashboard-menu.json';
+
+  export default {
+    data: () => ({
+      menu: DashboardMenu,
+    }),
+    methods: {
+      showDropdown(name) {
+        document.getElementById(name).classList.add('show');
+        document.getElementById(`menu${name}`).classList.add('show');
+      },
+      hideDropdown(name) {
+        document.getElementById(name).classList.remove('show');
+        document.getElementById(`menu${name}`).classList.remove('show');
+      }
+    }
+  }
+</script>
