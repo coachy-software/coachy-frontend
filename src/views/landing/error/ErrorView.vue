a52cefae18f58a43a50c2aaae4310a7459e61729
bmstefanski <bstefanskicom@gmail.com>
2019-01-22 11:36:48 +0100

    404 Error page (Closes #11)

---
+++ src/views/landing/error/ErrorView.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .page
+    .page-content
+      .container.text-center
+        .display-1.text-muted.mb-5
+          i.si.si-exclamation
+          |  400
+        h1.h2.mb-3 Oops.. You just found an error page..
+        p.h4.text-muted.font-weight-normal.mb-7 We are sorry but your request contains bad syntax and cannot be fulfilled…
+        button.btn.btn-primary(@click="$router.back()")
+          i.fe.fe-arrow-left.mr-2
+          | Go back
+</template>
+
+<style scoped>
+  @import url('~tabler-ui/dist/assets/css/dashboard.css');
+</style>
--- src/views/landing/routes.js
+++ src/views/landing/routes.js
@@ -1,13 +1,18 @@
-export default [{
-  path: '/',
-  component: () => import('@/views/landing/LandingLayout'),
-  children: [
-    {
-      path: '',
-      name: 'landing',
-      meta: {requiresAuth: true},
-      component: () => import('@/views/landing/home/HomeView')
-    }
-  ],
-  alias: ['/home']
-}];
+export default [
+  {
+    path: '/',
+    component: () => import('@/views/landing/LandingLayout'),
+    children: [
+      {
+        path: '',
+        name: 'landing',
+        meta: {requiresAuth: true},
+        component: () => import('@/views/landing/home/HomeView')
+      }
+    ],
+    alias: ['/home']
+  },
+  {
+    path: '*',
+    component: () => import('@/views/landing/error/ErrorView')
+  }];
