a4cb4d2d83eebfcef6372dd8bef39d8270e0eb3b
bmstefanski <bstefanskicom@gmail.com>
2019-01-26 18:10:41 +0100

    Translate menu messages and an error page

--- src/views/landing/error/ErrorView.vue
+++ src/views/landing/error/ErrorView.vue
@@ -4,9 +4,9 @@
       .container.text-center
         .display-1.text-muted.mb-5
           i.si.si-exclamation
-          |  400
+          |  404
         h1.h2.mb-3 Oops.. You just found an error page..
-        p.h4.text-muted.font-weight-normal.mb-7 We are sorry but your request contains bad syntax and cannot be fulfilled…
+        p.h4.text-muted.font-weight-normal.mb-7 We are sorry but our service is currently not available&hellip;
         button.btn.btn-primary(@click="$router.back()")
           i.fe.fe-arrow-left.mr-2
           | Go back
