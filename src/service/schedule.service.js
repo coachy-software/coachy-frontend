03cd671e213c9cb9f7ff988be936cf7b403a5fa9
bmstefanski <bstefanskicom@gmail.com>
2019-03-07 21:29:43 +0100

    Improve refresh feature in schedules view

--- src/service/schedule.service.js
+++ src/service/schedule.service.js
@@ -4,29 +4,47 @@ import {API_URL} from "@/utils/constants";
 import store from '@/store'
 
 export function fetchAll() {
-  return new Promise((resolve, reject) => {
-    store.commit(SET_STATUS, LOADING, {root: true});
+  store.commit(SET_STATUS, LOADING, {root: true});
 
-    let userIdentifier = JSON.parse(localStorage.getItem('user')).identifier;
-    let token = localStorage.getItem('token');
+  let userIdentifier = JSON.parse(localStorage.getItem('user')).identifier;
+  let token = localStorage.getItem('token');
+  let config = {headers: {'Authorization': `Basic ${token}`}};
+  let promises = [];
 
-    axios.get(`${API_URL}/schedules?charge.identifier=${userIdentifier}`, {
-      headers: {'Authorization': `Basic ${token}`}
-    })
-    .then(response => {
-      if (response.data) {
-        localStorage.setItem('schedules', JSON.stringify(response.data.content));
-        store.commit("schedule/SET_SCHEDULES", response.data.content);
-      }
+  let endpoints = [
+    `${API_URL}/schedules?charge.identifier=${userIdentifier}`,
+    `${API_URL}/schedules?creator.identifier=${userIdentifier}`
+  ];
 
-      store.commit(SET_STATUS, NOT_LOADING, {root: true});
-      resolve(response);
-    })
-    .catch(error => {
-      store.commit(SET_STATUS, NOT_LOADING, {root: true});
-      reject(error);
-    });
+  return new Promise((resolve) => {
+    for (let i = 0; i < endpoints.length; i++) {
+      let requestPromise = axios.get(endpoints[i], config)
+      .then(response => {
+        return response.data.content;
+      });
 
+      promises.push(requestPromise);
+    }
+
+    return resolve(Promise.all(promises));
+  })
+  .then(arrays => {
+    let chargeSchedules = arrays[0];
+    let creatorSchedules = arrays[1];
+
+    if (creatorSchedules.length > 0) {
+      creatorSchedules.forEach(schedule => {
+        let index = chargeSchedules.findIndex(chargeSchedule => chargeSchedule.identifier === schedule.identifier);
+
+        if (index === -1) {
+          chargeSchedules = chargeSchedules.concat(schedule);
+        }
+      });
+    }
+
+    localStorage.setItem('schedules', JSON.stringify(chargeSchedules));
+    store.commit("schedule/SET_SCHEDULES", chargeSchedules);
+    store.commit(SET_STATUS, NOT_LOADING, {root: true});
   });
 }
 
