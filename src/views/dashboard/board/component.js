968a3ae9185c3c0eccde176957e4e7821c3e2e4f
bmstefanski <bstefanskicom@gmail.com>
2019-03-14 22:40:24 +0100

    Add missing translations

--- src/views/dashboard/board/component.js
+++ src/views/dashboard/board/component.js
@@ -1,8 +1,18 @@
 import Task from './component/task/Task';
+import draggable from 'vuedraggable'
+
 
 export default {
+  data: () => ({
+    labels: [
+      {name: "Do zrobienia", tasks: []},
+      {name: "W trakcie", tasks: []},
+      {name: "Ukończone", tasks: []}
+    ]
+  }),
   name: 'tasks',
   components: {
-    Task
+    Task,
+    draggable
   }
 }
