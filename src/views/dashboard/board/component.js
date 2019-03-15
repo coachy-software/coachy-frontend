6921990e559def3dbcded5c8985f0d7d0e9254ba
bmstefanski <bstefanskicom@gmail.com>
2019-03-15 10:21:08 +0100

    Improve draggable elements and board template

--- src/views/dashboard/board/component.js
+++ src/views/dashboard/board/component.js
@@ -1,13 +1,38 @@
 import Task from './component/task/Task';
 import draggable from 'vuedraggable'
 
-
 export default {
   data: () => ({
     labels: [
-      {name: "Do zrobienia", tasks: []},
-      {name: "W trakcie", tasks: []},
-      {name: "Ukończone", tasks: []}
+      {
+        name: "Do zrobienia", tasks: [
+          {name: "Test", content: "test content", color: "#2b2b2b"},
+          {name: "Test1", content: "test content", color: "#2b2b2b"},
+          {name: "Test3", content: "test content", color: "#2b2b2b"}
+        ]
+      },
+      {
+        name: "W trakcie", tasks: [
+          {name: "XD", content: "test content", color: "#2b2b2b"},
+          {name: "XD1", content: "test content", color: "#2b2b2b"},
+          {name: "XD3", content: "test content", color: "#2b2b2b"},
+          {name: "XD4", content: "test content", color: "#2b2b2b"}
+        ]
+      },
+      {
+        name: "Ukończone", tasks: [
+          {name: "DDD", content: "test content", color: "#2b2b2b"},
+          {name: "DDD1", content: "test content", color: "#2b2b2b"},
+          {name: "DDD3", content: "test content", color: "#2b2b2b"}
+        ]
+      },
+      {
+        name: "Ukończone", tasks: [
+          {name: "DDD", content: "test content", color: "#2b2b2b"},
+          {name: "DDD1", content: "test content", color: "#2b2b2b"},
+          {name: "DDD3", content: "test content", color: "#2b2b2b"}
+        ]
+      }
     ]
   }),
   name: 'tasks',
