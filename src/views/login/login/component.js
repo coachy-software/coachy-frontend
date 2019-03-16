95644a7c61892bc0514bcf90317b8e451eaf79d2
bmstefanski <bstefanskicom@gmail.com>
2019-03-16 18:59:40 +0100

    Fetch schedules after login

--- src/views/login/login/component.js
+++ src/views/login/login/component.js
@@ -1,6 +1,7 @@
 import {notification} from "@/utils/toastr.utils";
 import {required} from "vuelidate/src/validators";
 import {NO_SPACE, NO_SPACE_AND_SPECIAL_CHARS} from "@/utils/constants";
+import {fetchAll} from "@/service/schedule.service";
 
 export default {
   name: 'login',
@@ -14,11 +15,15 @@ export default {
       .then(() => {
         let location = this.$route.query.redirect || '/dashboard';
         this.$router.push(location);
+        this.initData();
         notification.success(this.$t('login.logged_in'));
       })
-      .catch(error => {
+      .catch(() => {
         notification.error(this.$t('login.bad_credentials'))
       })
+    },
+    initData() {
+      fetchAll();
     }
   },
   computed: {
