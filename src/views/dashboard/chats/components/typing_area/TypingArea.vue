5f8a910077803e1dd5a203e66466c39128a1b070
bmstefanski <bstefanskicom@gmail.com>
2019-04-27 18:56:04 +0200

    Remove redundant components from chats view

--- src/views/dashboard/chats/components/typing_area/TypingArea.vue
+++
@@ -1,4 +0,0 @@
-<template lang="pug" src="./template.pug"></template>
-<script src="./component.js"></script>
-<style scoped src="../../style.css"></style>
-<style scoped src="../../responsive.css"></style>
