27fd6f95fd50d0fbdf8dd11f48975266584b418b
bmstefanski <bstefanskicom@gmail.com>
2019-05-28 14:16:40 +0200

    Limit modal images size to 100px in schedule-edit-modal

--- src/view/dashboard/schedule/component/edit_modal/ExerciseEditModal.vue
+++ src/view/dashboard/schedule/component/edit_modal/ExerciseEditModal.vue
@@ -1,2 +1,11 @@
 <template lang="pug" src="./template.pug"></template>
 <script src="./component.js"></script>
+<style scoped>
+  @media (min-width: 720px) {
+    .modal-image {
+      width: 100px;
+      height: 100px;
+      object-fit: cover;
+    }
+  }
+</style>
