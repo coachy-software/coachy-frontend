c8fef0f1fa279b6691ac8623105c6f1ccf43343c
bmstefanski <bstefanskicom@gmail.com>
2019-06-02 15:14:29 +0200

    Add prototype of notification dropdown

--- src/view/dashboard/component/NotificationDropdown.vue
+++ src/view/dashboard/component/NotificationDropdown.vue
@@ -4,23 +4,53 @@
       i.fe.fe-bell
       span.nav-unread
     .dropdown-menu.dropdown-menu-right.dropdown-menu-arrow(:class="{'show': this.$parent.dropdowns.notification.open}")
-      div(v-for="value in notifications")
-        router-link.dropdown-item.d-flex(:to="value.link")
-          span.avatar.mr-3.align-self-center(:style="{'background-image': `url(${value.avatar})`}")
-          div
-            strong {{value.author}}
-            |  {{value.message}}
-            .small.text-muted {{value.time}}
+      div(v-if="notifications.length === 0")
+        p.text-center.mt-2 {{$t('dropdowns.no_notifications')}}
+      div(v-else, v-for="notification in notifications")
+        template(v-if="notification.type === 'ALERT'")
+          router-link.dropdown-item.d-flex(to="javascript:;")
+            span.avatar.mr-3.align-self-center(:style="{'background-image': `url(${logo})`}")
+            div
+              | {{notification.content}}
+              .small.text-muted {{notification.createdAt | moment}}
+        template(v-else)
+          router-link.dropdown-item.d-flex(to="javascript:;")
+            span.avatar.mr-3.align-self-center(:style="{'background-image': `url(${logo})`}")
+            div
+              strong {{notification.senderName}}
+              |  {{notification.content}}
+              .small.text-muted {{notification.createdAt | moment}}
       .dropdown-divider
-      router-link.dropdown-item.text-center.text-muted-dark(to='/') {{$t('dropdowns.mark_all')}}
+      router-link.dropdown-item.text-center.text-muted-dark(to="/") {{$t('dropdowns.see_all')}}
 </template>
+<style scoped>
+  .dropdown-item.active, .dropdown-item:active {
+    color: #fff;
+    text-decoration: none;
+    background-color: #f8f9fa !important;
+  }
 
+  .dropdown-menu {
+    min-width: 30rem;
+  }
+</style>
 <script>
-  import notificationMock from '@/assets/mock/notification-mock.json';
+  import {subscribe} from "@/service/ws";
+  import moment from "moment";
+  import logo from "@/assets/dark-logo.svg";
 
   export default {
     data: () => ({
-      notifications: notificationMock
-    })
+      notifications: [],
+      logo: logo
+    }),
+    created() {
+      subscribe('/user/queue/notifications', (output) => this.notifications.push(JSON.parse(output.body)));
+    },
+    filters: {
+      moment: (date) => {
+        return moment(date).format('DD-MM-YYYY HH:mm:ss');
+      }
+    }
   }
 </script>
