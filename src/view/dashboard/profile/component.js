7ac56f2d689348d3c5aad15c8c6c0d020752b86d
bmstefanski <bstefanskicom@gmail.com>
2019-07-02 15:17:37 +0200

    Adjust recommendations fetching to backend corrections

--- src/view/dashboard/profile/component.js
+++ src/view/dashboard/profile/component.js
@@ -55,32 +55,7 @@ export default {
       .then(response => {
         ProfileService.fetchFollowers({identifier: id}).then(response => this.followers = response.data.followers);
         ProfileService.fetchFollowing({identifier: id}).then(response => this.following = response.data.following);
-        ProfileService.fetchRecommendations({identifier: id}).then(response => {
-          this.isLoading = true;
-
-          let newRecommendations = response.data;
-          newRecommendations.forEach((recommendation, index) => {
-            this.$store.dispatch('user/get', {identifier: recommendation.from}).then(response => {
-              let result = response.data;
-
-              recommendation.avatar = result.avatar;
-              recommendation.fromUsername = result.username;
-            }).then(() => {
-              if (index === newRecommendations.length - 1) {
-                this.updateRecommendations(newRecommendations);
-                this.isLoading = false;
-              }
-            });
-          });
-          
-          this.isLoading = false;
-        });
-
-        this.$store.dispatch('user/get', {identifier: id})
-        .then(response => {
-          ({username: this.username, displayName: this.displayName, avatar: this.avatar} = response.data);
-          this.isLoading = false;
-        });
+        ProfileService.fetchRecommendations({identifier: id}).then(response => this.recommendations = response.data);
 
         let result = response.data;
         let user = JSON.parse(localStorage.getItem('user'));
@@ -89,6 +64,9 @@ export default {
         this.isProfileOwner = result.userId === user.identifier;
 
         ({
+          username: this.username,
+          displayName: this.displayName,
+          avatar: this.avatar,
           website: this.website,
           title: this.title,
           bio: this.bio,
@@ -97,6 +75,8 @@ export default {
           services: this.services,
           socialLinks: this.socialLinks
         } = result);
+
+        this.isLoading = false;
       }).catch(() => {
         this.isLoading = false;
         this.$router.back()
